7f1485fd78818654dd10508a92ac36222c03bcf7
Douglas Koerich <douglas.koerich@utp.br>
2019-02-26 21:27:09 -0300

    Primeiro programa em C.
    Signed-off-by: Douglas Koerich <douglas.koerich@utp.br>

--- primprog.c
+++ primprog.c
@@ -1,16 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-
-#define PI 3.14
 
 int main() {
-    printf("Meu primeiro programa em C\n");
-
-    float area;
-    area = PI * pow(5, 2);
-    printf("A area do circulo de raio 5 eh %f\n", area);
-
+    printf("Meu primeiro programa em C!\n");
     return EXIT_SUCCESS;
 }
 
