f7041716d980d5f6de6f200567fa0713913b4b4e
Douglas Koerich <douglas.koerich@utp.br>
2019-08-01 21:55:13 -0300

    Meu primeiro programa em C
    Signed-off-by: Douglas Koerich <douglas.koerich@utp.br>

---
+++ primeiro.c
@@ -0,0 +1,14 @@
+/* Este eh um comentario que descreve o proposito deste programa
+em algumas linhas:
+- Ele imprime uma mensagem no console (terminal);
+- Termina retornando indicacao de "sucesso" para o sistema operacional. */
+
+// Este eh um comentario que se resume a uma linha apenas (nao continua na proxima)
+
+#include <stdio.h> // Inclui a biblioteca-padrao necessaria para uso de 'puts'
+
+int main() { // o { marca o inicio de um bloco de codigo
+    puts("Meuprimeiro programa em C na UTP!"); // puts imprime no console
+    return 0;
+}
+
