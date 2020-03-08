9550cb014e062e36fa2349ed3d6a0570796d74bd
Douglas Koerich <douglas.koerich@utp.br>
2020-03-07 23:29:33 -0300

    Primeiro programa em C.
    Signed-off-by: Douglas Koerich <douglas.koerich@utp.br>

---
+++ prim.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Essa eh uma biblioteca-padrao da linguagem C
+
+// Todo programa em C tem como ponto de entrada essa funcao main()
+int main() {
+    printf("Meu primeiro programa em C, yuhuu!\n");
+    /* A proxima linha de codigo mostra que
+    podemos colocar qualquer numero de espacos
+    entre dois elementos de um comando */
+    return          0;
+}
