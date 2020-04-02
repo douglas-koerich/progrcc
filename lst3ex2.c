733b1c9b09a7da6f9e3b38e50e4fb1770ccfe44e
Douglas Koerich <douglas.koerich@utp.br>
2020-04-01 21:56:36 -0300

    Mesmo exercicio, agora com for e break.
    Signed-off-by: Douglas Koerich <douglas.koerich@utp.br>

--- lst3ex2.c
+++ lst3ex2.c
@@ -3,16 +3,22 @@
 
 int main() {
 
-    int contador = 1; // inicializa o contador de passagens com a primeira (1)
-                      // a ser executada
+    int contador;
+
     unsigned acumulador = 0; // inicializa com o elemento neutro da soma
     unsigned menor, maior;
 
-    while (contador <= 5) {
+    // while (contador <= 5) {
+    for (contador = 1; contador <= 5; ++contador) {
         unsigned int numero;
 
-        printf("Digite o proximo numero: ");
+        printf("Digite o proximo numero, ou 0 para interromper: ");
         scanf("%d", &numero);
+
+        if (numero == 0) {
+            break; // interrompe a execucao do passo e sai do laco
+                   // (mesmo assim o ++contador lah em cima eh executado)
+        }
 
         if (contador == 1) { // eh a primeira passagem do laco?
             menor = maior = numero; // inicia a "memoria" do menor/maior
@@ -25,15 +31,25 @@ int main() {
             }
         }
 
+        /*
         //contador = contador + 1;
         //contador += 1;
         ++contador; // contador++;
+        */
 
         acumulador += numero;
     }
 
+    /*
     float media = acumulador / 5.0; // forca a constante ser real para a divisao
                                     // ser real tambem (e NAO inteira)
+    */
+    // Com a introducao do 'break' no laco, a media tem que ser calculada com
+    // base no contador; mas como a contagem considerou a digitacao do '0', que
+    // foi usado soh pra interromper o laco, vamos usar o valor do contador-1
+    float media = (float) acumulador / (contador-1); // a variavel 'acumulador' eh
+                                                     // usada na expressao como se
+                                                     // fosse um 'float'
 
     printf("O maior valor digitado foi %u, o menor foi %u e a media eh %.2f\n",
            maior, menor, media);
